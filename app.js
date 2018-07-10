d90d4ac3a1c54b6e255b2be182f62d32334691f6
Craig Booker <info@itech.plus>
2018-07-10 15:53:45 -0500

    Added the https module to the project to fix and error.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out .
+// Require 
+const https = require('https');
 const username = "Booker";
 
 // Function to print message to console.
